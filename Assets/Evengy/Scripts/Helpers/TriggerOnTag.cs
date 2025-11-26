00b17e14271c92b249695804a83d11756fd4b8b3
Rbocarro <33599920+Rbocarro@users.noreply.github.com>
2025-11-26 23:50:21 +0000

    scene stetup

--- Assets/Evengy/Scripts/Helpers/TriggerOnTag.cs
+++ Assets/Evengy/Scripts/Helpers/TriggerOnTag.cs
@@ -10,6 +10,20 @@ namespace Evengy.GridBasedMovementController.Helpers
 
         bool isTriggered;
 
+        SphereCollider triggerCollider;
+        private Color gizmoColor;
+
+        private void OnValidate()
+        {
+            triggerCollider = GetComponent<SphereCollider>();
+            SetGizmoColorFromTag();
+        }
+
+        private void Awake()
+        {
+            SetGizmoColorFromTag();
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.tag.Equals(triggerTag.ToString()))
@@ -27,5 +41,39 @@ namespace Evengy.GridBasedMovementController.Helpers
                 isTriggered = false;
             }
         }
+
+        private void SetGizmoColorFromTag()
+        {
+            switch (triggerTag)
+            {
+                case Tag.Tile:
+                    gizmoColor = Color.green;
+                    break;
+                case Tag.Obstacle:
+                    gizmoColor = Color.red;
+                    break;
+                case Tag.Player:
+                    // Blue for Player
+                    gizmoColor = Color.blue;
+                    break;
+                default:
+                    gizmoColor = Color.white;
+                    break;
+            }
+            gizmoColor.a = 0.7f;// Add a little transparency so it looks like a wireframe
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (triggerCollider != null)
+            {
+                Gizmos.color = gizmoColor;
+                Matrix4x4 oldMatrix = Gizmos.matrix;// Apply the object's transform to the Gizmo drawing
+                Gizmos.matrix = transform.localToWorldMatrix;
+                Gizmos.DrawWireSphere(triggerCollider.center, triggerCollider.radius);
+                Gizmos.matrix = oldMatrix;// Reset the matrix to prevent it from affecting other Gizmos
+            }
+        }
+
     }
 }
